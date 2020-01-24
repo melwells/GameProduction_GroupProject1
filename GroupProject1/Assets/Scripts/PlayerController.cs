b67a2f044964338d66e909fd5c4dfcd18256b957
melwells <55035851+melwells@users.noreply.github.com>
2020-01-24 18:12:31 -0500

    Movement

--- GroupProject1/Assets/Scripts/PlayerController.cs
+++ GroupProject1/Assets/Scripts/PlayerController.cs
@@ -5,17 +5,61 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-  public float speed = 0.4f;
+  private Rigidbody2D rb2d;
+
+  public float speed = 5.0f;
+
+  private Vector2 direction = Vector2.zero;
 
     // Start is called before the first frame update
     void Start()
     {
+      //rb2d = GetComponent<Rigidbody2D>();
 
+    }
+
+    void Update()
+    {
+      PlayerInput();
+
+      Move (direction);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+
+
+    }
+
+//find Input
+    void PlayerInput()
+    {
+      if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+      {
+        direction = Vector2.left;
+      }
+
+      else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+      {
+        direction =  Vector2.right;
+      }
+
+      else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+      {
+        direction = Vector2.up;
+      }
+
+      else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+      {
+        direction = Vector2.down;
+      }
+    }
+
+//determining movement direction
+    void Move(Vector2 d)
+    {
+      transform.localPosition += (Vector3)d * speed * Time.deltaTime;
 
     }
 }
